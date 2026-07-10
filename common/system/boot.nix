9852a694b4be68ef838b2080135b3ccb36914cb5
Aijaz Mohammad <20248043+gnudoc@users.noreply.github.com>
2026-07-10 09:26:56 +0100

    Another attempt to fix the resume from hibernate freezes

--- common/system/boot.nix
+++ common/system/boot.nix
@@ -5,6 +5,14 @@
     efi.canTouchEfiVariables = true;
   };
 
+  # Workaround for Wayland resume freezes - systemd v256 exposed a bunch of kernel bugs
+  systemd.services = {
+    systemd-suspend.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-hybrid-sleep.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+    systemd-suspend-then-hibernate.environment.SYSTEMD_SLEEP_FREEZE_USER_SESSIONS = "false";
+  };
+
   services.logind.settings.Login.HandleLidSwitch = "suspend-then-hibernate";
   systemd.sleep.settings.Sleep = {
     HibernateDelaySec = "5m";
